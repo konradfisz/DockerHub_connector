8abcfee8cb307d414a574860bddc7b22bb44ed6f
konradfisz <38684489+konradfisz@users.noreply.github.com>
2018-12-06 12:23:51 +0100

    Update main.java

--- main.java
+++ main.java
@@ -4,9 +4,11 @@ public class main {
 
 	public static void main (String[] args) {
         
-      String url = "jdbc:msql://10.0.10.3:3306/pwcho";
-	    String username = "kfisz";
-	    String password = "password";
+      	String url = "jdbc:msql://10.0.10.3:3306/pwcho";
+	String username = "kfisz";
+	String password = "password";
+		
+	System.out.println("Connecting to the database");
     
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
             System.out.println("Database conneted");
